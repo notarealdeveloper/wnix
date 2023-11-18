7db9949d6e75c6a8b297480f6563ae0da6caa6aa
Jason Wilkes <notarealdeveloper@gmail.com>
2023-11-18 14:52:50 +0000

    Mini project #1: merge mmry and embd refactor.
    Most of lib/middle is now implemented in the
    embd and mmry libraries. Let's use this as a
    test of how well the 'dst' strategy works.

--- src/wnix/lib/middle/__init__.py
+++ src/wnix/lib/middle/__init__.py
@@ -1,5 +1,5 @@
-from .cache import *
-from .embed import *
-from .space import *
-from .libgrep import *
-from .coreutils import *
+from mmry.cache import Cache as CacheDefault
+from embd.embed import Embed as EmbedDefault
+from embd.space import Space, think
+from embd.types import List, Dict, Object
+from embd.types import grep, greps, format_grep
