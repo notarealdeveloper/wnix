2e839522dfaf26ce115d08c422f3f849f3f0420f
Jason Wilkes <notarealdeveloper@gmail.com>
2023-11-21 10:57:44 -0800

    Rename Grep to grep2 because OSX can't read.

--- tests/test_grep_shell.py
+++ tests/test_grep_shell.py
@@ -4,7 +4,7 @@ import os
 
 def test_grep_shell():
 
-    cmd = "echo baseball basketball rugby soccer | fmt -1 | Grep -n 1 'european,american' sports"
+    cmd = "echo baseball basketball rugby soccer | fmt -1 | grep2 -n 1 'european,american' sports"
     out = os.popen(cmd).read().splitlines()
 
     assert out == [
